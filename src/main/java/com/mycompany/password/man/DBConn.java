4871726c5ebb78ea7e513d73991bde5787f50acc
Edward Chvainickas <chvainickas@gmail.com>
2024-10-27 19:19:02 +0000

    added dotenv plus dependencies

--- src/main/java/com/mycompany/password/man/DBConn.java
+++ src/main/java/com/mycompany/password/man/DBConn.java
@@ -1,25 +1,16 @@
 /*
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
-
-package com.mycompany.password.man;
-
 import io.github.cdimascio.dotenv.Dotenv;
 
-/**
- *
- * @author ed
- */
-public class PasswordMan {
+public class DBConn {
+        Dotenv dotenv = Dotenv.load();
 
-    public static void main(String[] args) {
-Dotenv dotenv = Dotenv.load();
-
-        // Get environment variables
         String dbUrl = dotenv.get("DB_URL");
         String dbUser = dotenv.get("DB_USER");
         String dbPassword = dotenv.get("DB_PASSWORD");
 
-        System.out.println("Database URL: " + dbUrl); // For testing
-    }
+
 }
+
